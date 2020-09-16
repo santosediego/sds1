4d019853a25f84bf70183995e81ed0f98fb03ac6
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-09-16 17:28:14 -0300

    Get records

--- backend/src/main/java/com/santosediego/dspesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/santosediego/dspesquisa/resources/RecordResource.java
@@ -1,10 +1,17 @@
 package com.santosediego.dspesquisa.resources;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santosediego.dspesquisa.dto.RecordDTO;
@@ -19,8 +26,34 @@ public class RecordResource {
 	private RecordService service;
 
 	@PostMapping
-	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
 		RecordDTO newDTO = service.insert(dto);
 		return ResponseEntity.ok().body(newDTO);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findAll(
+			@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+		// Converter a data minima e máxima;
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+
+		// Condicionar a quantidade de registros por página;
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+
+		// Configurar o objeto de paginação;
+		PageRequest pageRequest = PageRequest.of(
+				page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+		return ResponseEntity.ok().body(list);
 	}
 }
