f3fa425b717bd489fe093090f6e1caeadce77e42
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-09-16 10:16:47 -0300

    Project created

---
+++ backend/src/test/java/com/santosediego/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
